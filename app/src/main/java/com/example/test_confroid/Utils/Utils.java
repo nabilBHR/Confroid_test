54cc5f68449947a0ea39385943da55628f195a8c
Yacine HADJAR <yacinehadjar@gmail.com>
2021-04-03 19:53:24 +0200

    merging_branches_with_massinisa

--- app/src/main/java/com/example/test_confroid/Utils/Utils.java
+++ app/src/main/java/com/example/test_confroid/Utils/Utils.java
@@ -9,13 +9,6 @@ import java.util.Map;
 
 public class Utils {
 
-    public static Bundle convertToBundle(Map<String, String> configuration) {
-        Bundle bundle = new Bundle();
-        for (Map.Entry<String, String> entry : configuration.entrySet()) {
-            bundle.putString(entry.getKey(), entry.getValue());
-        }
-        return bundle;
-    }
 
     public static String getJsonString(Map<String, String> configuration) {
         JSONObject json = new JSONObject();
