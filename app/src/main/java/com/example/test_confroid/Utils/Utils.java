36a81917bbc92a13883bf75aac395815c422dfea
Yacine HADJAR <yacinehadjar@gmail.com>
2021-04-03 19:54:16 +0200

    merging_branches_with_massinisa

--- app/src/main/java/com/example/test_confroid/Utils/Utils.java
+++ app/src/main/java/com/example/test_confroid/Utils/Utils.java
@@ -1,25 +1,29 @@
 package com.example.test_confroid.Utils;
 
-import android.os.Bundle;
+import com.google.gson.Gson;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Utils {
-
 
     public static String getJsonString(Map<String, String> configuration) {
         JSONObject json = new JSONObject();
         for (Map.Entry<String, String> entry : configuration.entrySet()) {
             try {
-                if (!entry.getKey().equals("configName"))
+                if (!entry.getKey().equals("configName") || !entry.getKey().equals("sent"))
                     json.put(entry.getKey(), entry.getValue());
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
         }
         return json.toString();
+    }
+
+    public static Map<String,String> jsonToMap(String json){
+        return new Gson().fromJson(json, HashMap.class);
     }
 }
